f7c57b2a5e4ddf8284d43922803ff002ebdeafbc
klwork <klwork_v@126.com>
2016-04-14 07:13:51 +0800

    init

--- klwork-vertx/src/main/resources/public/readme.md
+++
@@ -1,6 +0,0 @@
-1.安装node相关
-npm instll
-2.安装前端依赖
-bower install
-3. 进行编译
-gulp
--- klwork-vertx/src/main/resources/public/tasks/clean.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-var del = require('del');
-var config = require('./config').build;
-
-module.exports = function() {
-  return function() {
-    return del([config.destination]);
-  };
-};
